e5bb06935cac858860f3cf584a9c46f600d0e73f
sapa73 <34308956+sapa73@users.noreply.github.com>
2018-02-26 13:51:37 +0200

    adding

---
+++ 2. Extract Sentences by Keyword/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _2.Extract_Sentences_by_Keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            string[] input = Console.ReadLine().Split(new char[] { '!', '.', '?' },
+                StringSplitOptions.RemoveEmptyEntries);
+            string pattern = @"[^A-Za-z]";
+            foreach (string item in input)
+            {
+                string[] words = Regex.Split(item, pattern);
+                if (input.Contains(word))
+                {
+                    Console.WriteLine(item.Trim());
+                }
+            }
+           ;
+        }
+    }
+}
